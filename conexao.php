c7f9beaf08b6824f667cd4128e9066e96e1815d0
Anderson Guilherme <86016646+andguilherme@users.noreply.github.com>
2022-08-09 23:09:52 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 ini_set('default_charset', 'iso-8859-1');
 $servidor = "localhost";
@@ -17,24 +16,4 @@ try{
 
 
 
-=======
-<?php
-ini_set('default_charset', 'iso-8859-1');
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "biblia";
-
-//$pdo = new PDO("mysql:host=$servidor; dbname=$banco", $usuario, $senha);
-try{
-   $pdo = new PDO("mysql:host=$servidor;dbname=$banco",$usuario,$senha); 
-   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-   //echo "Banco conectado com sucesso!"; 
-}catch(PDOException $erro){
-    echo "Falha ao se conectar com o banco! ";
-}
-
-
-
->>>>>>> 878c6f277bbf0a424309562c48617547c5431ecf
 ?>
